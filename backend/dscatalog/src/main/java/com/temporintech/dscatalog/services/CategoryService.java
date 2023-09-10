75761cfa58fab29bf8e0a816283bd0484518cf24
CauaneTemporin <temporin.cauane@gmail.com>
2023-09-10 20:07:33 -0300

    Delete Category

--- backend/dscatalog/src/main/java/com/temporintech/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/services/CategoryService.java
@@ -1,29 +1,34 @@
 package com.temporintech.dscatalog.services;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.temporintech.dscatalog.DTO.CategoryDTO;
 import com.temporintech.dscatalog.entities.Category;
 import com.temporintech.dscatalog.repositories.CategoryRepository;
-import com.temporintech.dscatalog.services.exceptions.EntityNotFoundException;
- 
+import com.temporintech.dscatalog.services.exceptions.DatabaseException;
+import com.temporintech.dscatalog.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
 
-	@Transactional(readOnly = true)//Garantia que vai ser uma transação com o BD, usando o readOnly não deixa sem travar
+	@Transactional(readOnly = true) // Garantia que vai ser uma transação com o BD, usando o readOnly não deixa sem
+									// travar
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
-		
+
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
@@ -41,4 +46,28 @@ public class CategoryService {
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
-}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+} 
