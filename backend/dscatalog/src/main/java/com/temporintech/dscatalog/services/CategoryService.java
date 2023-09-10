e0dd900482767e86351f5dcdbd149f63ffe91990
CauaneTemporin <temporin.cauane@gmail.com>
2023-09-10 17:01:48 -0300

    Category by id, exception handling

--- backend/dscatalog/src/main/java/com/temporintech/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/services/CategoryService.java
@@ -1,6 +1,8 @@
 package com.temporintech.dscatalog.services;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.temporintech.dscatalog.DTO.CategoryDTO;
 import com.temporintech.dscatalog.entities.Category;
 import com.temporintech.dscatalog.repositories.CategoryRepository;
+import com.temporintech.dscatalog.services.exceptions.EntityNotFoundException;
  
 @Service
 public class CategoryService {
@@ -22,5 +25,12 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
