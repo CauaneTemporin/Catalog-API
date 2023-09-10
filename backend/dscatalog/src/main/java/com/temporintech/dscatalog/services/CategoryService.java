56c418c081961f283c398c7ae9b1ba4f5dba4be7
CauaneTemporin <temporin.cauane@gmail.com>
2023-09-10 16:02:16 -0300

    CategoryDTO

--- backend/dscatalog/src/main/java/com/temporintech/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/services/CategoryService.java
@@ -1,14 +1,16 @@
 package com.temporintech.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.temporintech.dscatalog.DTO.CategoryDTO;
 import com.temporintech.dscatalog.entities.Category;
 import com.temporintech.dscatalog.repositories.CategoryRepository;
-
+ 
 @Service
 public class CategoryService {
 
@@ -16,7 +18,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)//Garantia que vai ser uma transação com o BD, usando o readOnly não deixa sem travar
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
