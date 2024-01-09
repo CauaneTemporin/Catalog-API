71fb1790daa7a71ff80646aaa3e3403c7b5b6112
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-09 20:13:28 -0300

    Testes nos pacotes repositories, services e test

---
+++ backend/dscatalog/src/test/java/com/temporintech/dscatalog/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.temporintech.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.temporintech.dscatalog.DTO.ProductDTO;
+import com.temporintech.dscatalog.entities.Category;
+import com.temporintech.dscatalog.entities.Product;
+
+public class Factory {
+
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good phone", 800.0, "https://img.com/img.png",
+				Instant.parse("2020-07-13T20:50:07.12345Z"));
+		product.getCategories().add(new Category(2L, "Eletrônicos"));
+		return product;
+	}
+
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+}
