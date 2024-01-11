3f184d17e07a44e34d3ae6e77efb0c277630480f
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-11 19:42:39 -0300

    Teste findAllPaged do ProductService

--- backend/dscatalog/src/test/java/com/temporintech/dscatalog/services/ProductServiceTests.java
+++ backend/dscatalog/src/test/java/com/temporintech/dscatalog/services/ProductServiceTests.java
@@ -13,10 +13,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.temporintech.dscatalog.DTO.ProductDTO;
 import com.temporintech.dscatalog.entities.Product;
 import com.temporintech.dscatalog.repositories.ProductRepository;
 import com.temporintech.dscatalog.services.exceptions.DatabaseException;
@@ -57,6 +60,14 @@ public class ProductServiceTests {
 		Mockito.doNothing().when(repository).deleteById(exintingId);
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+	}
+
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageable);
+		Assertions.assertNotNull(result);
+		Mockito.verify(repository).findAll(pageable);
 	}
 
 	@Test
