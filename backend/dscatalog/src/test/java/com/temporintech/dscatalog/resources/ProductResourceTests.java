449cd64041d121f723ef4ed79bdcd3b9a90b9ba6
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-14 10:36:42 -0300

    Teste na camada web update

--- backend/dscatalog/src/test/java/com/temporintech/dscatalog/resources/ProductResourceTests.java
+++ backend/dscatalog/src/test/java/com/temporintech/dscatalog/resources/ProductResourceTests.java
@@ -1,9 +1,11 @@
 package com.temporintech.dscatalog.resources;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
@@ -17,6 +19,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.temporintech.dscatalog.DTO.ProductDTO;
 import com.temporintech.dscatalog.services.ProductService;
 import com.temporintech.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -30,6 +33,9 @@ public class ProductResourceTests {
 
 	@MockBean
 	private ProductService productService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	private long nonExistingId;
 	private long exintingId;
@@ -47,6 +53,9 @@ public class ProductResourceTests {
 
 		Mockito.when(productService.findById(exintingId)).thenReturn(productDTO);
 		Mockito.when(productService.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
+		Mockito.when(productService.update(eq(exintingId), any())).thenReturn(productDTO);
+		Mockito.when(productService.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
 	}
 
 	@Test
@@ -68,6 +77,28 @@ public class ProductResourceTests {
 	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
 		ResultActions result = mockMvc.perform(
 				get("/products/{id}", nonExistingId).accept(org.springframework.http.MediaType.APPLICATION_JSON));
+		result.andExpect(status().isNotFound());
+	}
+
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		ResultActions result = mockMvc.perform(put("/products/{id}", exintingId).content(jsonBody)
+				.contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+				.accept(org.springframework.http.MediaType.APPLICATION_JSON));
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
+	}
+
+	@Test
+	public void updateShouldReturnNotFoundWthenIdDoesNotExist() throws Exception {
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		ResultActions result = mockMvc.perform(put("/products/{id}", nonExistingId).content(jsonBody)
+				.contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+				.accept(org.springframework.http.MediaType.APPLICATION_JSON));
 		result.andExpect(status().isNotFound());
 	}
 
