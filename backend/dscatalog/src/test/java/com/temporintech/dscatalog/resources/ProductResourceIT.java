4214b69ed6e1b5a70485e00e5808874173970697
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-15 16:46:01 -0300

    Teste na classe ProductServiceIT

--- backend/dscatalog/src/test/java/com/temporintech/dscatalog/resources/ProductResourceIT.java
+++ backend/dscatalog/src/test/java/com/temporintech/dscatalog/resources/ProductResourceIT.java
@@ -1,39 +1,86 @@
 package com.temporintech.dscatalog.resources;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.temporintech.dscatalog.repositories.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.temporintech.dscatalog.DTO.ProductDTO;
+import com.temporintech.dscatalog.tests.Factory;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class ProductResourceTestsIT {
-
+public class ProductResourceIT {
 	@Autowired
 	private MockMvc mockMvc;
-	
-	@Autowired
-	private ProductRepository repository;
 
-	private long nonExistingId;
-	private long exintingId;
-	private long countTotalProduct;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		exintingId = 1L;
+		existingId = 1L;
 		nonExistingId = 1000L;
-		countTotalProduct = 25L;
+		countTotalProducts = 25L;
 	}
-	
+
 	@Test
-	private void findAllShouldReturnSortedPageWhenShortByName() throws Exception {
-		
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+
+		ResultActions result = mockMvc
+				.perform(get("/products?page=0&size=12&sort=name,asc").accept(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+		result.andExpect(jsonPath("$.content").exists());
+		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+		result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+	}
+
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+		ProductDTO productDTO = Factory.createProductDTO();
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+		String expectedName = productDTO.getName();
+		String expectedDescription = productDTO.getDescription();
+
+		ResultActions result = mockMvc.perform(put("/products/{id}", existingId).content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(existingId));
+		result.andExpect(jsonPath("$.name").value(expectedName));
+		result.andExpect(jsonPath("$.description").value(expectedDescription));
+	}
+
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+
+		ProductDTO productDTO = Factory.createProductDTO();
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+		ResultActions result = mockMvc.perform(put("/products/{id}", nonExistingId).content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isNotFound());
 	}
 }
