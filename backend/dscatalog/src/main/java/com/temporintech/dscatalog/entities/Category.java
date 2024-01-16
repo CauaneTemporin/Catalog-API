708bc0f13d8aae2e149e8cd1b6401ef767c62024
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-16 20:32:34 -0300

    Produtos associados a uma categoria

--- backend/dscatalog/src/main/java/com/temporintech/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/entities/Category.java
@@ -1,37 +1,42 @@
 package com.temporintech.dscatalog.entities;
 
-
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
+public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
-	
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+
 	public Category() {
-		
+
 	}
 
 	public Category(Long id, String name) {
@@ -62,15 +67,19 @@ public class Category implements Serializable{
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
-	
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
 	}
-	
+
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
+	}
+
+	public void setProducts(Set<Product> products) {
+		this.products = products;
 	}
 
 	@Override
@@ -89,6 +98,5 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
