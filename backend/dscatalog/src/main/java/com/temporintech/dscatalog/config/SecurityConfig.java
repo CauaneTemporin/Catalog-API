8c768908ac67bdd8cc3194ac7a668a29cdff6673
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-20 23:45:48 -0300

    SecurityConfig

--- backend/dscatalog/src/main/java/com/temporintech/dscatalog/config/SecurityConfig.java
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.temporintech.dscatalog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**");
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(requests -> requests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+		return http.build();
 	}
+
 }
