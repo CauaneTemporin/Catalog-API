b75d601b54bf1315fb1662dc5cc0d6f1d6fcef34
CauaneTemporin <temporin.cauane@gmail.com>
2024-01-25 17:06:34 -0300

    Implementando o WebSecurityConfig

---
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.temporintech.dscatalog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+A
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(requests -> requests.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+		return http.build();
+	}
+
+	//PORQUE NÃO FUNCIONA?
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+//	}
+
+}
