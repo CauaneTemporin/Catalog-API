91639ee8d4eb763224c14db2e76e7ff789742b3d
CauaneTemporin <temporin.cauane@gmail.com>
2024-02-03 19:01:29 -0300

    Configuração para liberar o H2

--- backend/dscatalog/src/main/java/com/temporintech/dscatalog/config/ResourceServerConfig.java
+++ backend/dscatalog/src/main/java/com/temporintech/dscatalog/config/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.temporintech.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -20,15 +22,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private JwtTokenStore tokenStore;
-	
+
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
-	
+
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
-	
-	private static final String[] ADMIN = { "/users/**" };	
+
+	private static final String[] ADMIN = { "/users/**" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -38,11 +40,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
-		.antMatchers(ADMIN).hasRole("ADMIN")
-		.anyRequest().authenticated();		
+		// H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
+				.permitAll().antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN").antMatchers(ADMIN)
+				.hasRole("ADMIN").anyRequest().authenticated();
 	}
+	
 }
